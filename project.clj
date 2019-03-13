9105023ed5491daa01cd0294301ed5feb0897b67
David James Humphreys <davidjameshumphreys@gmail.com>
2019-03-13 21:28:26 +0000

    Bump groovy dependency & fix transients

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject lein-groovyc "0.2.0"
+(defproject org.clojars.djh/lein-groovyc "0.3.0"
   :description "Compile groovy sources"
-  :dependencies [[org.codehaus.groovy/groovy-all "1.7.10" ] [lancet "1.0.1"]]
+  :dependencies [[org.codehaus.groovy/groovy-all "2.5.6" :extension "pom"]
+                 [lancet "1.0.2" :exclusions [org.apache.ant/ant]]]
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :sign-releases false}]]
   :eval-in-leinigen true)
